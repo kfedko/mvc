a659fed70d11c7a51b8536cc37de5c0270c134ba
Kyryl Fedko <kfedko@ascendix.com>
2019-02-13 20:23:12 +0200

    Custom http handler has been implemented

---
+++ MVC/MVC/App_Start/RouteConfig.cs
@@ -0,0 +1,36 @@
+﻿using MVC.Handlers;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVC
+{
+	public class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			//#httpHandler option #1 with Web.config
+			//routes.IgnoreRoute("handler/{*path}");
+
+			//#httpHandler option #2
+			routes.Add(new Route("handler/{*path}", new CustomRouteHandler()));
+
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+			);
+		}
+
+		class CustomRouteHandler : IRouteHandler
+		{
+			//#httpHandler
+			public IHttpHandler GetHttpHandler(RequestContext requestContext)
+			{
+				return new UserInfoHttpHandler();
+			}
+		}
+	}
+}
---
+++ MVC/MVC/Handlers/UserInfoHttpHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+
+namespace MVC.Handlers
+{
+	public class UserInfoHttpHandler : IHttpHandler
+	{
+		public bool IsReusable => false;
+
+		public void ProcessRequest(HttpContext context)
+		{
+			string result = "<p>Your IP: " + context.Request.UserHostAddress + "</p>";
+			result += "<p>UserAgent: " + context.Request.UserAgent + "</p>";
+			context.Response.Write(result);
+		}
+	}
+}
