3558ed42379e580a66df0532bff52bae831234ec
Kyryl Fedko <kfedko@ascendix.com>
2019-02-13 20:32:30 +0200

    Implemented custom http module

---
+++ MVC/MVC/Modules/TimerModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web;
+
+namespace MVC.Modules
+{
+	public class TimerModule : IHttpModule
+	{
+		private Stopwatch timer;
+
+		private void HandleBeginRequest(object src, EventArgs args) {
+			timer = Stopwatch.StartNew();
+		}
+		private void HandleEndRequest(object src, EventArgs args) {
+			HttpContext context = HttpContext.Current;
+			context.Response.Write(string.Format(
+				"<div style='color:red;'>Time for request processing is: {0:F5} seconds</div>",
+				((float)timer.ElapsedTicks) / Stopwatch.Frequency));
+		}
+		public void Init(HttpApplication context)
+		{
+			context.BeginRequest += HandleBeginRequest;
+			context.EndRequest += HandleEndRequest;
+		}
+
+		public void Dispose(){}
+	}
+}
